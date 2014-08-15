a82f58d94d771889a4591550e7cadc3ae0cbdfc5
Desiree Santos <dsantos@thoughtworks.com>
2014-08-15 01:45:34 -0300

    Command cli for temcafe project

---
+++ test/message_test.js
@@ -0,0 +1,30 @@
+var assert = require("assert")
+var coffee = require('../lib/messages');
+
+describe('When does not has cofee', function(){
+    it('level is zero', function(){
+      	assert.equal(coffee.message('0'), 'No coffee');
+    })
+
+    it('level is 1', function(){
+      	assert.equal(coffee.message('10'), 'No coffee');
+    })
+})
+
+describe('When cofee is finishing', function(){
+    it('level is 20', function(){
+      	assert.equal(coffee.message('20'), 'Run the coffee is finishing');
+    })
+})
+
+describe('Has coffee', function(){
+    it('level is 50', function(){
+      	assert.equal(coffee.message('50'), 'Has coffee');
+    })
+})
+
+describe('When the cofee is fresh', function(){
+    it('level is 100', function(){
+      	assert.equal(coffee.message('100'), 'Fresh coffee');
+    })
+})
